1c13f8d07ce2a76fd9ec06b849ff03731eb3e4c9
Dennis Chan <dchan@dmcouncil.org>
2024-03-29 14:13:05 -0400

    Add missing dot files

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+      ],
+    },
+  },
+  plugins: ['ember'],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {
+    'ember/no-global-jquery': 0,
+    'ember/no-jquery': 0
+  },
+  globals: {
+    '$': 'readonly'
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        './.eslintrc.js',
+        './.stylelintrc.js',
+        './.template-lintrc.js',
+        './ember-cli-build.js',
+        './index.js',
+        './testem.js',
+        './blueprints/*/index.js',
+        './config/**/*.js',
+        './tests/dummy/config/**/*.js',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      extends: ['plugin:n/recommended'],
+    },
+    {
+      // test files
+      files: ['tests/**/*-test.{js,ts}'],
+      extends: ['plugin:qunit/recommended'],
+      rules: {
+        'qunit/require-expect': 0
+      }
+    },
+  ],
+};
